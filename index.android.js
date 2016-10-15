b5371012f09e416122855957342e9b09ee5990d1
Quyen <boy20195@gmail.com>
2016-10-15 20:14:54 +0700

    Flexbox and array styles

--- index.android.js
+++ index.android.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {
     Text,
     View,
-    AppRegistry
+    AppRegistry,
+    StyleSheet
 } from 'react-native';
 
 class StopWatch extends Component {
@@ -26,17 +27,48 @@ class StopWatch extends Component {
       );
     }
 
+    border(color) {
+      return {
+        borderColor: color,
+        borderWidth: 4
+      }
+    }
+
     render() {
        return (
-         <View>
-             <Text>
-               00:00:00
-             </Text>
-             {this.startStopButton()}
-             {this.lapButton()}
+         <View style={styles.container}>
+             <View style={[styles.header, this.border('yellow')]}>
+                <View style={this.border('red')}>
+                  <Text>
+                    00:00:00
+                  </Text>
+                </View>
+                <View style={this.border('green')}>
+                  {this.startStopButton()}
+                  {this.lapButton()}
+                </View>
+             </View>
+             <View style={[styles.footer, this.border('blue')]}>
+               <Text>
+                 I am a list of Laps
+               </Text>
+             </View>
          </View>
        );
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Fill the entire the screen
+    alignItems: 'stretch', // Child max width
+  },
+  header: { // Yellow
+    flex: 1,
+  },
+  footer: { // Footer
+    flex: 1,
+  }
+});
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch);
